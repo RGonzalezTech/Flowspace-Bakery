e33bb7da9087c49df37cbe300119c9ec01468493
Raul Gonzalez <info@rgonzalez.tech>
2021-11-12 19:50:55 -0800

    read `ready` column from `#ready?

--- app/models/cookie.rb
+++ app/models/cookie.rb
@@ -1,9 +1,14 @@
 class Cookie < ActiveRecord::Base
   belongs_to :storage, polymorphic: :true
-  
   validates :storage, presence: true
 
+  attribute :ready, :boolean, default: false
+
   def ready?
-    true
+    return self.ready
+  end
+
+  def set_ready(new_ready)
+    update(ready: new_ready)
   end
 end
