50689307465f39c174016d0ebe428942dbe765cc
Raul Gonzalez <info@rgonzalez.tech>
2021-11-12 20:39:56 -0800

    Test BakerWorker sets cookies in oven to ready

--- spec/workers/baker_worker_spec.rb
+++ spec/workers/baker_worker_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
-RSpec.describe BakerWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+# set type to :feature so we can leverage session helpers
+RSpec.describe BakerWorker, type: :feature do
+  before(:each) do
+    @user = create_and_signin
+    @oven = @user.ovens.first
+  end
+
+  it "prepares cookies" do
+    @oven.create_cookie!(fillings: "Chocolate")
+    new_cookie = @oven.cookie
+    expect(new_cookie.ready?).to be false
+
+    # Execute immediately
+    BakerWorker.new.perform(@oven.id)
+
+    new_cookie.reload
+    expect(new_cookie.ready?).to be true
+  end
 end
